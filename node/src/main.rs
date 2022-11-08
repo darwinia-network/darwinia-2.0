8a3e947e3b5966e5f938b11656358e805b7263a9
Xavier Lau <xavier@inv.cafe>
2022-11-08 13:36:27 +0800

    Skeleton

---
+++ node/src/main.rs
@@ -0,0 +1,14 @@
+//! Darwinia parachain node CLI.
+
+#![warn(missing_docs)]
+
+mod chain_spec;
+#[macro_use]
+mod service;
+mod cli;
+mod command;
+mod rpc;
+
+fn main() -> sc_cli::Result<()> {
+	command::run()
+}
