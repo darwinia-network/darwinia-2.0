c480c1224f1ce238f894a47896446e74f0a30bde
Xavier Lau <xavier@inv.cafe>
2022-11-21 13:20:50 +0800

    Take storages

--- tool/state-processor/src/main.rs
+++ tool/state-processor/src/main.rs
@@ -1,8 +1,12 @@
+mod type_registry;
+use type_registry::*;
+
 // std
 use std::{env, fs::File, io::Read};
 // crates.io
 use anyhow::Result;
 use fxhash::FxHashMap;
+use parity_scale_codec::Decode;
 // hack-ink
 use subspector::ChainSpec;
 
@@ -10,22 +14,26 @@ fn main() -> Result<()> {
 	env::set_var("RUST_LOG", "state_processor");
 	pretty_env_logger::init();
 
-	let state = State::from_file("test-data/darwinia-node-export.json")?
-		.prune(b"Babe", None)?
-		.prune(b"Timestamp", None)?
-		.prune(b"TransactionPayment", None)?
-		.prune(b"Authorship", None)?
-		.prune(b"ElectionProviderMultiphase", None)?
+	let mut accounts_infos = <Vec<AccountInfo>>::new();
+	let _state = State::from_file("test-data/darwinia-node-export.json")?
+		.take(b"System", b"Account", &mut accounts_infos)
+		.prune(b"Babe", None)
+		.prune(b"Timestamp", None)
+		.prune(b"TransactionPayment", None)
+		.prune(b"Authorship", None)
+		.prune(b"ElectionProviderMultiphase", None)
 		// TODO
-		.prune(b"Offences", None)?
-		.prune(b"Historical", None)?
+		.prune(b"Offences", None)
+		.prune(b"Historical", None)
 		// TODO
-		.prune(b"Session", None)?
-		.prune(b"Grandpa", None)?
-		.prune(b"ImOnline", None)?
-		.prune(b"AuthorityDiscovery", None)?
-		.prune(b"DarwiniaHeaderMmr", None)?
-		.prune(b"Democracy", None)?;
+		.prune(b"Session", None)
+		.prune(b"Grandpa", None)
+		.prune(b"ImOnline", None)
+		.prune(b"AuthorityDiscovery", None)
+		.prune(b"DarwiniaHeaderMmr", None)
+		.prune(b"Democracy", None);
+
+	dbg!(accounts_infos);
 
 	Ok(())
 }
@@ -41,12 +49,11 @@ impl State {
 		Ok(Self(serde_json::from_slice::<ChainSpec>(&v)?.genesis.raw.top))
 	}
 
-	fn prune(mut self, pallet: &[u8], items: Option<&[&[u8]]>) -> Result<Self> {
+	fn prune(mut self, pallet: &[u8], items: Option<&[&[u8]]>) -> Self {
 		// Prune specific storages.
 		if let Some(items) = items {
 			for item in items {
-				let k = substorager::storage_key(pallet, item);
-				let k = array_bytes::bytes2hex("0x", &k.0);
+				let k = item_key(pallet, item);
 
 				self.0.remove(&k).or_else(|| {
 					log::warn!(
@@ -61,8 +68,7 @@ impl State {
 		}
 		// Prune entire pallet.
 		else {
-			let prefix = subhasher::twox128(pallet);
-			let prefix = array_bytes::bytes2hex("0x", &prefix);
+			let prefix = pallet_key(pallet);
 			let mut pruned = false;
 
 			self.0.retain(|full_key, _| {
@@ -80,6 +86,49 @@ impl State {
 			}
 		}
 
-		Ok(self)
+		self
 	}
+
+	fn take<T>(mut self, pallet: &[u8], item: &[u8], buffer: &mut Vec<T>) -> Self
+	where
+		T: Decode,
+	{
+		let item_key = item_key(pallet, item);
+
+		self.0.retain(|full_key, v| {
+			if full_key.starts_with(&item_key) {
+				match decode(v) {
+					Ok(v) => buffer.push(v),
+					Err(e) => log::warn!("failed to decode `{full_key}:{v}`, due to `{e}`"),
+				}
+
+				false
+			} else {
+				true
+			}
+		});
+
+		self
+	}
+}
+
+fn pallet_key(pallet: &[u8]) -> String {
+	let prefix = subhasher::twox128(pallet);
+
+	array_bytes::bytes2hex("0x", &prefix)
+}
+
+fn item_key(pallet: &[u8], item: &[u8]) -> String {
+	let k = substorager::storage_key(pallet, item);
+
+	array_bytes::bytes2hex("0x", &k.0)
+}
+
+fn decode<T>(hex: &str) -> Result<T>
+where
+	T: Decode,
+{
+	let v = array_bytes::hex2bytes(hex).map_err(|e| anyhow::anyhow!("{e:?}"))?;
+
+	Ok(T::decode(&mut &*v)?)
 }
---
+++ tool/state-processor/src/type_registry.rs
@@ -0,0 +1,23 @@
+// crates.io
+use parity_scale_codec::Decode;
+
+pub type Nonce = u32;
+pub type Balance = u128;
+
+pub type RefCount = u32;
+
+#[derive(Debug, Decode)]
+pub struct AccountInfo {
+	pub nonce: Nonce,
+	pub consumers: RefCount,
+	pub providers: RefCount,
+	pub sufficients: RefCount,
+	pub data: AccountData,
+}
+#[derive(Debug, Decode)]
+pub struct AccountData {
+	pub free: Balance,
+	pub reserved: Balance,
+	pub free_kton: Balance,
+	pub reserved_kton: Balance,
+}
