8b3bc5e954632e047b2ebd975fe154bdf11c26df
bear <boundless.forest@outlook.com>
2022-12-21 11:46:45 +0800

    Fix revert

--- node/src/chain_spec/pangolin.rs
+++ node/src/chain_spec/pangolin.rs
@@ -154,12 +154,7 @@ pub fn genesis_config() -> ChainSpec {
 				balances: Default::default(),
 				transaction_payment: Default::default(),
 				assets: AssetsConfig {
-					assets: vec![(
-						AssetIds::PKton as _,
-						ROOT,
-						true,
-						1,
-					)],
+					assets: vec![(AssetIds::PKton as _, ROOT, true, 1)],
 					metadata: vec![(
 						AssetIds::PKton as _,
 						b"Pangolin Commitment Token".to_vec(),
