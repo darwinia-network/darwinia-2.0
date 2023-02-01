27e8adaeeb423904fc04f68f98e8346aedbdbf00
bear <boundless.forest@outlook.com>
2023-02-01 15:33:18 +0800

    Test opcodes

--- tests/tests.ts
+++ tests/tests.ts
@@ -1,7 +1,8 @@
 // Make sure that the tests are performed in the correct order.
-// import "./ethereum/test-code";
-// import "./ethereum/test-balance";
+import "./ethereum/test-code";
+import "./ethereum/test-balance";
 import "./ethereum/test-nonce";
-// import "./ethereum/test-contract";
-// import "./ethereum/test-constants";
-// import "./ethereum/test-block";
+import "./ethereum/test-contract";
+import "./ethereum/test-constants";
+import "./ethereum/test-block";
+import "./ethereum/test-opcodes";
