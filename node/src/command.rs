309683ab9f66afdc6b85bf94299ff8c6cb194cbb
Guantong <i@guantong.io>
2023-02-02 23:00:25 +0800

    Compile done

--- node/src/command.rs
+++ node/src/command.rs
@@ -577,7 +577,7 @@ pub fn run() -> Result<()> {
 					if config.role.is_authority() { "yes" } else { "no" }
 				);
 
-				if collator_options.relay_chain_rpc_url.is_some() && !cli.relay_chain_args.is_empty() {
+				if !collator_options.relay_chain_rpc_urls.is_empty() && !cli.relay_chain_args.is_empty() {
 					log::warn!("Detected relay chain node arguments together with --relay-chain-rpc-url. This command starts a minimal Polkadot node that only uses a network-related subset of all relay chain CLI options.");
 				}
 
