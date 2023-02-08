229251e4a4bfc3a52b1b261c2f1aebe7ec27c65a
Guantong <i@guantong.io>
2023-02-08 13:46:04 +0800

    Companion for paritytech/cumulus#2057

--- runtime/crab/src/lib.rs
+++ runtime/crab/src/lib.rs
@@ -602,7 +602,7 @@ sp_api::impl_runtime_apis! {
 
 	#[cfg(feature = "try-runtime")]
 	impl frame_try_runtime::TryRuntime<Block> for Runtime {
-		fn on_runtime_upgrade(checks: bool) -> (Weight, Weight) {
+		fn on_runtime_upgrade(checks: frame_try_runtime::UpgradeCheckSelect) -> (Weight, Weight) {
 			// substrate
 			use frame_support::log;
 
