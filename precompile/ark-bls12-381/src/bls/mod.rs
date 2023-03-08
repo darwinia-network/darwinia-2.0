2dd20622ecd204c7d9cf2d9f1d4eb9999b5a7b31
echo <hujw77@gmail.com>
2023-03-08 15:37:26 +0800

    Add license

--- precompile/ark-bls12-381/src/bls/mod.rs
+++ precompile/ark-bls12-381/src/bls/mod.rs
@@ -1,3 +1,23 @@
+// This file is part of Darwinia.
+//
+// Copyright (C) 2018-2023 Darwinia Network
+// SPDX-License-Identifier: GPL-3.0
+//
+// Darwinia is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Darwinia is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Darwinia. If not, see <https://www.gnu.org/licenses/>.
+//
+// Inspired from https://github.com/w3f/apk-proofs/blob/main/bw6/src/bls/mod.rs
+
 use ark_bls12_381::{
 	g2::Config as G2Config, Bls12_381, G1Affine, G1Projective, G2Affine, G2Projective,
 };
